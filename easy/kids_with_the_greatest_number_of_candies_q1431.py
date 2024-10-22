f0527dca8ff56bc8a5790cd2a4602888482b1c7a
yiyuan5 <yiyuan.li1@student.unimelb.edu.au>
2024-10-22 18:59:05 +1100

    upload solution for q1431

---
+++ easy/kids_with_the_greatest_number_of_candies_q1431.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_candies = max(candies)
+        result = []
+        
+        for candy in candies:
+            result.append(candy + extraCandies >= max_candies)
+        
+        return result
