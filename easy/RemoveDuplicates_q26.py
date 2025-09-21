8cf0a5ea3327d7589c81d357a36092af7ef34402
Yiyuan Li <48795728+yiyuanlee@users.noreply.github.com>
2025-09-21 22:51:07 +1000

    Implement removeDuplicates q26

---
+++ easy/RemoveDuplicates_q26.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+
+        j = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[j - 1]:
+                nums[j] = nums[i]
+                j += 1
+
+        return j
